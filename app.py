28b075bcdfc769a5d0fcb4096ccad5c478036add
Gianni <gianniicherry@gmail.com>
2023-10-31 17:01:51 -0700

    initial flask/react setup

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  # SQLite database URL
+db = SQLAlchemy(app)
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+class Story(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=False)
+    character = db.relationship('Character', backref=db.backref('stories', lazy=True))
+
+@app.route('/create-db')
+def create_db():
+    db.create_all()  # Creates the database and tables based on the models
+    return 'Database created successfully!'
+
+if __name__ == '__main__':
+    app.run()
