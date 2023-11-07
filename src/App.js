05418a46ba571119b52a6a11037e7ceb2c2da23f
Gianni <gianniicherry@gmail.com>
2023-11-07 15:37:23 -0800

    working role change options

--- src/App.js
+++ src/App.js
@@ -1,36 +1,59 @@
 import './App.css';
-import {useState, useEffect} from 'react'
-
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [characterName, setCharacterName] = useState("")
-  const [classType, setClassType] = useState("Warrior")
-  const [weapon, setWeaponType] = useState("")
-  const [roles, setRoles] = useState([])
+  const [characterName, setCharacterName] = useState('');
+  const [classType, setClassType] = useState('');
+  const [weapon, setWeaponType] = useState('');
+  const [roles, setRoles] = useState([]);
+  const [roleDescription, setRoleDescription] = useState('');
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/api/roles')  // Adjust the URL to match your Flask route
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error('Error fetching roles:', error));
+    fetch('http://127.0.0.1:5000/api/roles')
+      .then((response) => response.json())
+      .then((data) => setRoles(data))
+      .catch((error) => console.error('Error fetching roles:', error));
   }, []);
+
+  const handleRoleChange = (e) => {
+    const selectedRoleId = e.target.value;
+    const selectedRole = roles.find((role) => role.id === parseInt(selectedRoleId));
+
+    if (selectedRole) {
+      setClassType(selectedRole.name);
+      setRoleDescription(selectedRole.description);
+    }
+  };
 
   return (
     <div className="App">
       <form>
-        <label>Enter your Character Name.</label>
-        <input type="text" value={characterName} onChange={(e) => setCharacterName(e.target.value)}></input>
-        <select id="classType" value={classType} onChange={(e)=> setClassType(e.target.value)}>
-          <option value="Warrior">Warrior</option>
-          <option value="Wizard">Wizard</option>
-          <option value="Assassin">Assassin</option>
-          <option value="Ranger">Ranger</option>
-          <option value="Templar">Templar</option>
-          <option value="Wicken">Wicken</option>
+        <label>Enter your Character Name:</label>
+        <input
+          type="text"
+          value={characterName}
+          onChange={(e) => setCharacterName(e.target.value)}
+        ></input>
+        <select
+          id="classType"
+          value={classType}
+          onChange={handleRoleChange}
+        >
+          <option value="">Select a role</option>
+          {roles.map((role) => (
+            <option key={role.id} value={role.id}>
+              {role.name}
+            </option>
+          ))}
         </select>
       </form>
+          {classType}
+      {roleDescription && (
+        <p>Selected Role Description: {roleDescription}</p>
+      )}
     </div>
   );
 }
 
 export default App;
+
